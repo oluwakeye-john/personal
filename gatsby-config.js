07b12a41d41af8cf80a0be8d8351bb1ed52fe6b2
oluwakeye-john <bjohnoluwakeye@gmail.com>
2020-08-09 20:13:53 +0100

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,7 +98,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-174454448-1`,
       },
     },
     {
